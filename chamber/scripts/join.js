1d04416b9043a19cadb0bce5ac7a85396fa47fac
xanamal <xanenthegreat@gmail.com>
2026-05-29 19:16:46 -0700

    added small thankyou page and added time stamp to join js

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,3 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("timestamp").value =
         new Date().toISOString();
 });
+
+document.querySelector("#timestamp").value = new Date().toLocaleString();
